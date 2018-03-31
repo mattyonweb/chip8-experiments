1aef3d4b79ef1cd512efc55d9ab9bf0bb6b7e593
mattyonweb <mattycaos@gmail.com>
2018-03-31 22:05:23 +0200

    avanti popolo

--- emu.c
+++ emu.c
@@ -57,6 +57,7 @@ int execute(Chip8 chip8) {
                     b2 = chip8->memory[chip8->pc] & 0xF,
                     b3 = (chip8->memory[chip8->pc+1] & 0xF0) >> 4,
                     b4 = chip8->memory[chip8->pc+1] & 0xF;
+    unsigned char   lastTwo   = (b3 << 4) | b4;
                     
     printf("0x%03X\t%01X%01X%01X%01X\t", chip8->pc,b1,b2,b3,b4);
 
@@ -95,50 +96,124 @@ int execute(Chip8 chip8) {
 
         case 3:
             printf("[SKIP_EQ]\tSKIP: ");
-            if (chip8->registers[ (instruction&0x0f00)>>8 ] == (instruction&0x00ff)) {
-                chip8->pc += 2;
-                printf("TRUE\t%02X==%02X\n", chip8->registers[ (instruction&0x0f00)>>8 ], instruction&0x00ff);
+            if (chip8->registers[b2] == lastTwo) {
+                chip8->pc += 2; 
+                printf("TRUE\t%02X==%02X\n", chip8->registers[b2], lastTwo);
             }
             else
-                printf("FALSE\t%02X!=%02X\n", chip8->registers[ (instruction&0x0f00)>>8 ], instruction&0x00ff);
+                printf("FALSE\t%02X!=%02X\n", chip8->registers[b2], lastTwo);
             break;
             
         case 4:
             printf("[SKIP_NE]\tSKIP: ");
-            if (chip8->registers[ (instruction&0x0f00)>>8 ] != (instruction&0x00ff)) {
-                chip8->pc += 2;
-                printf("TRUE\t%02X!=%02X\n", chip8->registers[ (instruction&0x0f00)>>8 ], instruction&0x00ff);
+            if (chip8->registers[b2] != (lastTwo)) {
+                chip8->pc += 2; 
+                printf("TRUE\t%02X!=%02X\n", chip8->registers[b2], lastTwo);
             }
             else
-                printf("FALSE\t%02X==%02X\n", chip8->registers[ (instruction&0x0f00)>>8 ], instruction&0x00ff);
+                printf("FALSE\t%02X==%02X\n", chip8->registers[b2], lastTwo);
             break;
 
         case 5:
             printf("[SKIP_RegE]\tSKIP: ");
-            if (chip8->registers[ (instruction&0x0f00)>>8 ] == chip8->registers[(instruction&0x00f0)>>4]) {
-                chip8->pc += 2;
-                printf("TRUE\tV%X==V%X (%x==%x)\n", chip8->registers[ (instruction&0x0f00)>>8 ], chip8->registers[(instruction&0x00f0)>>4]);
-            }
+            if (chip8->registers[b2] == chip8->registers[b3]) {
+                chip8->pc += 2; 
+                printf("TRUE\t%02X==%02X\n", chip8->registers[b2], chip8->registers[b3]);
+            } 
             else
-                printf("FALSE\t%02X!=%02X\n", chip8->registers[ (instruction&0x0f00)>>8 ], chip8->registers[(instruction&0x00f0)>>4]);
+                printf("FALSE\t%02X!=%02X\n", chip8->registers[b2], chip8->registers[b3]);
             break;
 
         case 6:
-            printf("[MOVE]\t");
-            chip8->registers[ (instruction&0x0f00)>>8 ] = instruction&0x00ff;
-            printf("V%X = 0x%x\n", (instruction&0x0f00)>>8, instruction&0x00ff);
+            printf("[LI]\t");
+            chip8->registers[b2] = lastTwo;
+            printf("V%X = 0x%x\n", b2, lastTwo);
             break;
             
         case 7:
             printf("[ADD_EQ]\t");
-            chip8->registers[ (instruction&0x0f00)>>8 ] += instruction&0x00ff;
-            printf("V%X += 0x%x\n", (instruction&0x0f00)>>8, instruction&0x00ff);
+            chip8->registers[b2] += lastTwo;
+            printf("V%X += 0x%x\n", b2, lastTwo);
             break;
 
+        case 8:
+            switch (b4) {
+                case 0:
+                    printf("[COPY]\tV%X <- V%X\n", b2, b3);
+                    chip8->registers[b2] = chip8->registers[b3];
+                    break;
+
+                case 1:
+                    printf("[OR]\tV%X |= V%X\n", b2, b3);
+                    chip8->registers[b2] = chip8->registers[b2] | chip8->registers[b3];
+                    break;
+
+                case 2:
+                    printf("[AND]\tV%X &= V%X\n", b2, b3);
+                    chip8->registers[b2] = chip8->registers[b2] & chip8->registers[b3];
+                    break;
+
+                case 3:
+                    printf("[XOR]\tV%X ^= V%X\n", b2, b3);
+                    chip8->registers[b2] = chip8->registers[b2] ^ chip8->registers[b3];
+                    break;
+
+                case 4:
+                    printf("[ADD_C]\tV%X += V%X (c)\n", b2, b3);
+                    if (chip8->registers[b2] + chip8->registers[b3] > 255)
+                        chip8->registers[0xF] = 1;
+                    chip8->registers[b2] += chip8->registers[b3];
+                    break;
+                    
+                case 5:
+                    printf("[SUB_C]\tV%X -= V%X (c)\n", b2, b3);
+                    if (chip8->registers[b2] - chip8->registers[b3] < 0)
+                        chip8->registers[0xF] = 1;
+                    chip8->registers[b2] -= chip8->registers[b3];
+                    break;
+
+                case 6:
+                    printf("[SHIFTR]\tV%X, V%X\n", b2, b3);
+                    chip8->registers[0xF] = chip8->registers[b3]&0b00000001; //GIUSTO?!
+                    chip8->registers[b3] = chip8->registers[b3] >> 1;
+                    chip8->registers[b2] = chip8->registers[b3];
+                    break;
+
+                case 7:
+                    printf("[SUB_C]\tV%X = V%X - V%X (c)\n", b2, b3, b2);
+                    if (chip8->registers[b3] - chip8->registers[b2] < 0)
+                        chip8->registers[0xF] = 1;
+                    chip8->registers[b2] = chip8->registers[b3] - chip8->registers[b2];
+                    break;
+
+                case 0xE:
+                    printf("[SHIFTL]\tV%X, V%X\n", b2, b3);
+                    chip8->registers[0xF] = chip8->registers[b3]&0b10000000; //GIUSTO?!
+                    chip8->registers[b3] = chip8->registers[b3] << 1;
+                    chip8->registers[b2] = chip8->registers[b3];
+                    break;
+                                        
+                default:
+                    printf("error\n");
+                    break;
+            }
+            break;
+
+        case 0xA:
+            printf("[SETI]\tI = %03x\n", (instruction & 0x0fff));
+            chip8->I = instruction & 0x0fff;
+            break;
+
+        case 0xB:
+            printf("[JUMP]\tPC = %03x\n", instruction & 0x0fff);
+            chip8->pc = instruction & 0x0fff;
+            updatePc = 0;
+            break;
+        
         case 0xC:
             printf("[RAND]\t");
-            chip8->registers[(instruction&0x0f00)>>8] = (rand()%255) & (instruction&0xFF);
-            printf("V%X = rand()\n", (instruction&0x0f00)>>8);
+            chip8->registers[b2] = (rand()%255) & (lastTwo);
+            printf("V%X = rand()\n", b2);
             break;
             
         default:
