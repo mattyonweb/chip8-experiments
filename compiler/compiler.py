654fbbcfc2235ef24f98ed592643efd64761706f
mattonweb <mattycaos@gmail.com>
2018-04-09 23:53:34 +0200

    Compilatori I

---
+++ compiler/compiler.py
@@ -0,0 +1,29 @@
+with open("test.chip8", "r") as f:
+    raw_lines = map(str.strip, f.readlines())
+
+src_lines = list()
+for l in raw_lines:
+    if l != "":
+        src_lines.append(l)
+
+def return_sections(stripped_lines):
+    try:
+        idx_data = stripped_lines.index(".data:")
+    except:
+        idx_data = -1
+        
+    try:
+        idx_code = stripped_lines.index(".code:")
+    except:
+        raise LookupError("Can't find .code tag")
+
+    if idx_data == -1:
+        data = stripped_lines[idx_data+1:idx_code]
+    else:
+        data = []
+
+    code = stripped_lines[idx_code+1:]
+
+    return data, code
+    
+data, code = return_sections(src_lines)
