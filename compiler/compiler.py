8656787506338a45c69e3c5db376e6a30fa30168
mattonweb <mattycaos@gmail.com>
2018-04-10 16:11:11 +0200

    Piccolo refact compilatore

--- compiler/compiler.py
+++ compiler/compiler.py
@@ -2,7 +2,6 @@ import opcodes as opc
 import translation as trans
 import linker as lk
 
-
 def read_source(filename):
     with open(filename, "r") as f:
         raw_lines = map(str.strip, f.readlines())
@@ -31,25 +30,17 @@ def return_sections(stripped_lines):
     code = stripped_lines[idx_code+1:]
 
     return data, code
-   
-src_lines = read_source("maze.chip8")
-data, code = return_sections(src_lines)
 
-out = list()
-for l in code:
-    translated = trans.translate_line(l)
-    out.append(translated)
-
-def return_spirit_table(lines):
-    """ Ritorna dizionario nomeSpirito : str(idx) (già base 16) """
+def return_spirit_table(data_lines, first_idx_free):
+    """ Traduce tutti gli spirits dichiarati nella sezione .data in un
+    dizionario di formato nomeSpirito : str(idx) in base 16; quindi 
+    ritorna il dizionario e i bytes degli spiriti formattati in hex """
     spiriti_table  = {}
     spiriti_values = []
     
-    first_idx_free = 512 + 2 * len(list(filter(lambda l: ":" not in l, out)))
-    
-    for spirit in data:
-        name, values = spirit.replace(": ", " : ").replace(" ", "").split(":")
-        bytes_str    = values.replace(", ", ",").split(",")
+    for spirit in data_lines:
+        name, values = spirit.replace(": ", " : ").replace(" ", "").split(":")  #nome: values
+        bytes_str    = values.replace(", ", ",").split(",")                     #b,y,t,e,s,_,s,t,r
         bytes_       = list(map(lambda b: "{:02X}".format(int(b,16)), bytes_str))
         
         spiriti_table[name] = "{:03X}".format(int(str(first_idx_free)))
@@ -58,18 +49,26 @@ def return_spirit_table(lines):
     
     spiriti_values = list(map(lambda b: int(b, 16), spiriti_values))
     return (spiriti_table, spiriti_values)
-    
-
-spirit_table, spirit_values = return_spirit_table(out)
-code_translated = lk.linker(out, spirit_table)
 
 def chunk_code(two_bytes_codes):
+    """ "AB4F" -> "AB", "4F" """
     bytes_ = []
     for i in range(0, len(code_translated)):
         bytes_.append(two_bytes_codes[i][:2])
         bytes_.append(two_bytes_codes[i][2:4])
     return list(map(lambda b: int(b, 16), bytes_))
     
+
+src_lines = read_source("maze.chip8")
+data, code = return_sections(src_lines)
+
+intermediate_trans = [trans.translate_line(l) for l in code]
+
+first_idx_free = 512 + 2 * len(list(filter(lambda l: ":" not in l, intermediate_trans)))
+
+spirit_table, spirit_values = return_spirit_table(data, first_idx_free)
+code_translated = lk.linker(intermediate_trans, spirit_table)
+
 bytecode = chunk_code(code_translated)
 
 output = bytearray(bytecode + spirit_values)
